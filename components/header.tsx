ab16b48d987a3e871d78faaaefc3f7fa3b475ae6
krativeweb <krativewebdeveloper@gmail.com>
2026-05-16 11:34:29 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
- 
+  
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
  
